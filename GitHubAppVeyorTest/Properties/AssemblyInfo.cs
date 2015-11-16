327e766e750943127d864dcb844388b284ed4c12
Johannes Valtonen <jvaltonen@gmail.com>
2015-11-16 12:58:34 +0200

    Update AssemblyInfo.cs

--- GitHubAppVeyorTest/Properties/AssemblyInfo.cs
+++ GitHubAppVeyorTest/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@
 [assembly: AssemblyDescription("Testing GitHub, AppVeyor and nuget.org integrations.")]
 [assembly: AssemblyCompany("johannesvaltonen")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
