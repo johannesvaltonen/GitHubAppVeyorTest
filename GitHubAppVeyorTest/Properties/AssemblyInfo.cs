8aca9a6ad336bbebc02aaee851248075bc902817
johannesvaltonen <johannes.valtonen@affecto.com>
2015-11-16 11:59:48 +0200

    Added author.

--- GitHubAppVeyorTest/Properties/AssemblyInfo.cs
+++ GitHubAppVeyorTest/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 
 [assembly: AssemblyTitle("GitHubAppVeyorTest")]
 [assembly: AssemblyDescription("Testing GitHub, AppVeyor and nuget.org integrations.")]
+[assembly: AssemblyCompany("johannesvaltonen")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
