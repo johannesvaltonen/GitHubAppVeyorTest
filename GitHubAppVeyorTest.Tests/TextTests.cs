22177b2c577c294f1c1043ecf11e29c9de8108a2
johannesvaltonen <johannes.valtonen@affecto.com>
2015-11-16 09:27:09 +0200

    Small test project.

---
+++ GitHubAppVeyorTest.Tests/TextTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitHubAppVeyorTest.Tests
+{
+    [TestClass]
+    public class TextTests
+    {
+        [TestMethod]
+        public void ToUpper()
+        {
+            const string text = "SomeThing";
+            string result = Text.ToUpper(text);
+
+            Assert.AreEqual(text.ToUpper(), result);
+        }
+
+        [TestMethod]
+        public void ToLower()
+        {
+            const string text = "SomeThing";
+            string result = Text.ToLower(text);
+
+            Assert.AreEqual(text.ToLower(), result);
+        }
+    }
+}
---
+++ GitHubAppVeyorTest/Text.cs
@@ -0,0 +1,15 @@
+﻿namespace GitHubAppVeyorTest
+{
+    public class Text
+    {
+        public static string ToUpper(string text)
+        {
+            return text.ToUpper();
+        }
+
+        public static string ToLower(string text)
+        {
+            return text.ToLower();
+        }
+    }
+}
